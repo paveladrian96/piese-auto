aca4713def309e0d85b1a96c12c6ca6b2830ecac
Adrian Pavel <adrian.pavel-ext@continental-corporation.com>
2020-11-14 16:48:24 +0200

    make it better

--- app.js
+++ app.js
@@ -63,6 +63,10 @@ const port = process.env.PORT || 8000
 
 if(process.env.NODE_ENV === "production") {
     app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
 }
 
 app.listen(port, () => console.log(`Listening to port: ${port}`))
